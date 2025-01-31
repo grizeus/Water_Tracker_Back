942d1f44cb2fe43959be9847e7d4e9c5e5f4a8cc
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-01-31 18:03:57 +0100

    add UserCollections

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,1 +1,55 @@
+import mongoose from 'mongoose';
+import { handleSaveError, setUpdateSettings } from '../helpers';
+import { genderList } from '../../constants/user';
 
+const { Schema, model } = mongoose;
+
+const authSchema = new Schema(
+  {
+    email: {
+      type: String,
+      // match: змінна,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      maxLength: 64,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: genderList,
+      default: 'woman',
+    },
+    name: {
+      type: String,
+      minLength: 3,
+      maxLength: 24,
+      default: function () {
+        return this.email; // Використовуємо email як значення за замовчуванням
+      },
+    },
+    // URL аватара користувача, за замовчуванням Cloudinary посилання
+    avatarURL: {
+      type: String,
+      default: 'https://res.cloudinary.com/',
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+authSchema.post('save', handleSaveError);
+authSchema.pre('findOneAndUpdate', setUpdateSettings);
+authSchema.post('findOneAndUpdate', handleSaveError);
+
+const UserCollections = model('user', authSchema);
+export default UserCollections;
