b0fbc853dc49bad5b7dbe063199da376e6a7d6e6
grizeus <zaytsewww@gmail.com>
2025-02-02 12:57:40 +0200

    add untested code

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import {
   addWaterEntry,
   updateWaterEntry,
@@ -28,8 +29,19 @@ export const updateWaterEntryController = async (req, res) => {
   })
 };
 
-// Видалення запису про випиту воду
-export const deleteWaterEntryController = async (req, res) => { };
+export const deleteWaterEntryController = async (req, res) => {
+
+  const {id: _id }= req.params;
+  const userId = req.user._id;
+
+  const result = await deleteWaterEntry({_id, userId});
+
+  if (!result) {
+    throw createHttpError(404, "Entry not found");
+  }
+
+  res.status(204).send();
+};
 
 // Отримання денної статистики
 export const getDailyWaterDataController = async (req, res) => { };
