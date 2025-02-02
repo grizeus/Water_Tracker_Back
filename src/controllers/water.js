f5dada3d9dd51435a8e0f7740485e7e567d77b4e
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-02-02 22:34:44 +0100

    fix

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -17,12 +17,11 @@ export const updateWaterEntryController = async (req, res) => {
   const { id: _id } = req.params;
   const { time, amount, userId } = req.body;
 
-
   const result = await updateWaterEntry(_id, { amount, time });
 
   console.log('Update result:', result);
 
-  if (!result) throw createHttpError(404, "User not found")
+  if (!result) throw createHttpError(404, 'User not found');
 
   res.status(200).json({
     data: result,
@@ -30,27 +29,19 @@ export const updateWaterEntryController = async (req, res) => {
   });
 };
 
-
-// Видалення запису про випиту воду
-export const deleteWaterEntryController = async (req, res) => {};
-
+// Видалення запису про воду
 export const deleteWaterEntryController = async (req, res) => {
-
-  const {id: _id } = req.params;
+  const { id: _id } = req.params;
   const userId = req.user._id;
 
-  const result = await deleteWaterEntry({_id, userId});
+  const result = await deleteWaterEntry({ _id, userId });
 
   if (!result) {
-    throw createHttpError(404, "Entry not found");
+    throw createHttpError(404, 'Entry not found');
   }
 
   res.status(204).send();
 };
-
-
-// Отримання денної статистики
-export const getDailyWaterDataController = async (req, res) => {};
 
 // Отримання місячної статистики
 export const getMonthlyWaterDataController = async (req, res) => {
