9efd64b6773569137bfae22056944ab7278b587d
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-02-08 22:01:20 +0200

    fix middlevare

--- src/middlewares/validateRequest.js
+++ src/middlewares/validateRequest.js
@@ -1,4 +1,4 @@
-export const validateRequest = (schema, source = 'body') => (req, res, next) => {
+export const validateRequest = (schema, source = 'params') => (req, res, next) => {
     const { error } = schema.validate(req[source]);
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
